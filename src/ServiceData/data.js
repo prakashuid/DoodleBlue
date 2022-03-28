c5ed236309e48e0d4638b1cad1e6447256b92e88
Anbarasan Selvam <anbarasan.selvam@toffeeinsurance.com>
2022-03-28 10:09:15 +0530

    Data file modified

--- src/ServiceData/data.js
+++ src/ServiceData/data.js
@@ -7,8 +7,7 @@ const data = [
     {id:12,name:'https://i.picsum.photos/id/1013/4256/2832.jpg?hmac=UmYgZfqY_sNtHdug0Gd73bHFyf1VvzFWzPXSr5VTnDA'},
     {id:13,name:'https://i.picsum.photos/id/1020/4288/2848.jpg?hmac=Jo3ofatg0fee3HGOliAIIkcg4KGXC8UOTO1dm5qIIPc'},
     {id:14,name:'https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y'},
-    {id:15,name:'https://i.picsum.photos/id/1033/2048/1365.jpg?hmac=zEuPfX7t6U866nzXjWF41bf-uxkKOnf1dDrHXmhcK-Q'},
-    {id:16,name: 'https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g'},
+    {id:15,name:'https://i.picsum.photos/id/1033/2048/1365.jpg?hmac=zEuPfX7t6U866nzXjWF41bf-uxkKOnf1dDrHXmhcK-Q'},    
     ]
     
     export default data;
